44ad92af9f4847d5acd94ea879834e6bb3f8055e
Bob <bobjacob23@yahoo.com>
2024-10-21 20:18:49 -0700

    created interface--not sure if right

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,4 +1,8 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 export default interface Candidate {
-
+  username: string,
+  location: string,
+  email: string,
+  company: string,
+  bio: string,
 }
